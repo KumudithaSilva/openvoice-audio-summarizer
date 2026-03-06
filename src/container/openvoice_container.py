0446f35bef54249ac698ab46b8ddad1bcd546582
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-03-06 23:26:34 +0530

    add create_chat_messages and create_chat_completion_service to OpenVoiceContainer

--- src/container/openvoice_container.py
+++ src/container/openvoice_container.py
@@ -1,7 +1,16 @@
+from components.chat_completion import ChatCompletionService
 from components.chat_connection import ChatConnectionService
+from components.chat_messages import MessageCompletionService
+from infrastructure.falcon_prompt import PromptProvider
+from infrastructure.falconai_service import FalconAIService
+from interfaces.chat.i_chat_history import IMessageCompletionService
+from interfaces.chat.i_oneshot_prompt import IPrompt
+from interfaces.chat.i_voice_complete import IVoiceCompletionService
 from interfaces.chat.i_voice_connection import IVoiceConnection
 from interfaces.infra.i_config_provider import IConfigProvider
 from infrastructure.config_provider import ConfigProvider
+from interfaces.llm.i_ai_client import IAIClient
+from interfaces.llm.i_falcon_operations import IFalconAIOperations
 
 
 class OpenVoiceContainer:
@@ -21,3 +30,21 @@ class OpenVoiceContainer:
             config_provider = ConfigProvider()
 
         return ChatConnectionService(config_provider)
+
+    def create_chat_messages(self) -> IMessageCompletionService:
+        """
+        Create and return a chat completion service.
+        """
+        prompt_provider: IPrompt = PromptProvider()
+        return MessageCompletionService(prompt_provider)
+
+    def create_chat_completion_service(
+        self,
+        ai_client: IAIClient,
+    ) -> IVoiceCompletionService:
+        """
+        Create and return a chat completion service using the
+        provided AI client.
+        """
+        falcon_service: IFalconAIOperations = FalconAIService(ai_client)
+        return ChatCompletionService(falcon_service)
