092e8c6a8fcd2f36a4e38dcc901dc00ac8c5fded
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-03-08 20:55:36 +0530

    add Interface transcription generation.

---
+++ src/interfaces/chat/i_voice_transcription.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import IO, Union
+from dto.audio_data import AudioData
+
+
+class IVoiceTranscriptionService(ABC):
+    """
+    Interface transcription generation.
+    """
+
+    @abstractmethod
+    def generate(self, audio_path: Union[str, IO[bytes]]) -> str:
+        """
+        Genereate transcription for client request
+
+        Args:
+            audio_path (str | IO[bytes]): Path to audio file or file-like object.
+
+        Returns:
+            str: Return response for AI
+        """
+        pass
