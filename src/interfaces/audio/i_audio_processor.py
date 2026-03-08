98b2cc85beb59f673640112ff906832b9d7f9eb9
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-03-08 20:53:53 +0530

    modify the audio process interface path to accept both str and io byte

--- src/interfaces/audio/i_audio_processor.py
+++ src/interfaces/audio/i_audio_processor.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import IO, Union
 from dto.audio_data import AudioData
 
 
 class IAudioProcessor(ABC):
 
     @abstractmethod
-    def load_audio_file(self, path: str) -> AudioData:
+    def load_audio_file(self, path: Union[str, IO[bytes]]) -> AudioData:
         pass
 
     @abstractmethod
