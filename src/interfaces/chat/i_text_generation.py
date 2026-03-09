4eaf1bc3f83d8c8d2c8fa92384fbda5b91d7e7a4
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-03-09 16:47:14 +0530

    modify the generate method parameter to str

--- src/interfaces/chat/i_text_generation.py
+++ src/interfaces/chat/i_text_generation.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Dict, List
 
 
 class ITextGenereateService(ABC):
@@ -8,7 +7,7 @@ class ITextGenereateService(ABC):
     """
 
     @abstractmethod
-    def generate(self, messages: List[Dict]) -> str:
+    def generate(self, voice_text: str) -> str:
         """
         Genereate response for client request
 
